43054cdae3f91825ee433c66887feefa9041fdf2
Justin.Nguyen <student.j.nguyen@horizoncolumbus.org>
2020-02-05 09:24:47 -0500

    table and adding orders

--- PizzaOrderingSystem.py
+++ PizzaOrderingSystem.py
@@ -11,37 +11,37 @@ def home():
             data_list = []
             if request.form['submit_button'] == 'Add Sicilian':
                 data_list.append('Sicilian')
-                data_list.append('11.99')
+                data_list.append(11.99)
                 conn.execute(command, data_list)
                 conn.commit()
             if request.form['submit_button'] == 'Add Marine':
                 data_list.append('Marine')
-                data_list.append('11.99')
+                data_list.append(11.99)
                 conn.execute(command, data_list)
                 conn.commit()
             if request.form['submit_button'] == 'Add Mixed':
                 data_list.append('Mixed')
-                data_list.append('11.99')
+                data_list.append(11.99)
                 conn.execute(command, data_list)
                 conn.commit()
             if request.form['submit_button'] == 'Add Napoliten':
                 data_list.append('Napoliten')
-                data_list.append('11.99')
+                data_list.append(11.99)
                 conn.execute(command, data_list)
                 conn.commit()
             if request.form['submit_button'] == 'Add Hot Dog':
                 data_list.append('Hot Dog')
-                data_list.append('4.99')
+                data_list.append(4.99)
                 conn.execute(command, data_list)
                 conn.commit()
             if request.form['submit_button'] == 'Add Meatball Dog':
                 data_list.append('Meatball Dog')
-                data_list.append('5.99')
+                data_list.append(5.99)
                 conn.execute(command, data_list)
                 conn.commit()
             if request.form['submit_button'] == 'Add Water Dog':
                 data_list.append('Water Dog')
-                data_list.append('0.99')
+                data_list.append(0.99)
                 conn.execute(command, data_list)
                 conn.commit()
     return render_template('home.html')
@@ -53,8 +53,12 @@ def cart():
         command1 = "SELECT * FROM orders"
         cursor1 = conn.execute(command1)
         orders_made = cursor1.fetchall()
-    if request.form['jeremiah'] == 'Place order':
-    return render_template('cart.html', orders_made=orders_made)
+        total = 0
+        for items in orders_made:
+            total = total+items[1]
+            round(total, 2)
+            total_price1 = total
+    return render_template('cart.html', orders_made=orders_made, total_price1=total_price1)
 
 
 if __name__ == "__main__":
